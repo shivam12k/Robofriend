951eb7346c20bd2e23ae344c9ef19f90229409e0
shivam <63141401+shivam12k@users.noreply.github.com>
2020-07-11 00:18:25 +0530

    Updates

---
+++ precache-manifest.714ec203a2980fb373e4672b28a5ddf8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6ec6eed0a462983bc55f862c118a0fe",
+    "url": "/Robofriend/index.html"
+  },
+  {
+    "revision": "97d18fd6cf8b9f035ffd",
+    "url": "/Robofriend/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "caff0fa030426d78b539",
+    "url": "/Robofriend/static/css/main.cd84915f.chunk.css"
+  },
+  {
+    "revision": "97d18fd6cf8b9f035ffd",
+    "url": "/Robofriend/static/js/2.526585eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robofriend/static/js/2.526585eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caff0fa030426d78b539",
+    "url": "/Robofriend/static/js/main.af691cdb.chunk.js"
+  },
+  {
+    "revision": "aa332650baad8ad9bc36",
+    "url": "/Robofriend/static/js/runtime-main.df88e8ff.js"
+  }
+]);
